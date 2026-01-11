71b7debac27d156c2cce48e03ecd421c88448e83
Nick Christensen <nick.g.christensen@gmail.com>
2026-01-10 20:20:08 -0600

    Change env flags from GLOBAL to ENVIRONMENT group

--- src/flags.ts
+++ src/flags.ts
@@ -3,11 +3,11 @@ import {Flags} from '@oclif/core'
 export const hostname = Flags.string({
   description: 'Paperless-ngx base URL',
   env: 'PPLS_HOSTNAME',
-  helpGroup: 'GLOBAL',
+  helpGroup: 'ENVIRONMENT',
 })
 
 export const token = Flags.string({
   description: 'Paperless-ngx API token',
   env: 'PPLS_TOKEN',
-  helpGroup: 'GLOBAL',
+  helpGroup: 'ENVIRONMENT',
 })
